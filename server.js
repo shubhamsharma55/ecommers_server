1c202b2cabe33210bd411aa6c6862cea8d0a8b2a
SHUBHAM SHARMA <111723264+shubhamsharma55@users.noreply.github.com>
2025-07-02 14:45:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cokieParser = require('cookie-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cokieParser());
 
+const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.json({ msg: "this is Example" });
 });
 
 app.listen(PORT, () => {
-  console.log("server is running...");
+  console.log("server is running");
 });
 
 app.use('/user',require('./routes/useRouter'))
 
 const URI = process.env.MONGODB_URL;
-
 
 mongoose.connect(URI, {
     // useCreateIndex: true,
